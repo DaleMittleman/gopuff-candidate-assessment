4c9d055a2224e7819dce86ce01e3da4911a34448
Dale Mittleman <dale.mittleman@sagentlending.com>
2020-03-25 16:11:05 -0400

    Adding starting .NET Core API project layout, doing some initial base cleanup, adding .gitignore to exclude build output

---
+++ Microservice.CartManager/Microservice.CartManager/Controllers/CartsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Microservice.CartManager.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CartsController : ControllerBase
+    {
+        private readonly ILogger<CartsController> _logger;
+
+        public CartsController(ILogger<CartsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            this._logger.Log(LogLevel.Information, "Called: GET /Carts");
+
+            return this.Ok();
+        }
+    }
+}
