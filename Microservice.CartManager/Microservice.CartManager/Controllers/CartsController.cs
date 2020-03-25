0ec9ad1edebf4e55218a6b04b3117418c11ffdcf
Dale Mittleman <dale.mittleman@sagentlending.com>
2020-03-25 18:17:48 -0400

    Adding distributed cache extensions for working with objects.  Fleshing out a basic cart API

--- Microservice.CartManager/Microservice.CartManager/Controllers/CartsController.cs
+++ Microservice.CartManager/Microservice.CartManager/Controllers/CartsController.cs
@@ -1,6 +1,11 @@
 ﻿namespace Microservice.CartManager.Controllers
 {
+    using System;
+    using System.Threading.Tasks;
+    using Microservice.CartManager.Extensions;
+    using Microservice.CartManager.Models;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Caching.Distributed;
     using Microsoft.Extensions.Logging;
 
     /// <summary>
@@ -11,26 +16,79 @@
     public class CartsController : ControllerBase
     {
         private readonly ILogger<CartsController> logger;
+        private readonly IDistributedCache cache;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="CartsController"/> class.
         /// </summary>
         /// <param name="logger">A logger for this controller.</param>
-        public CartsController(ILogger<CartsController> logger)
+        /// <param name="cache">The distributed cache (redis).</param>
+        public CartsController(
+            ILogger<CartsController> logger,
+            IDistributedCache cache)
         {
             this.logger = logger;
+            this.cache = cache;
         }
 
         /// <summary>
         /// Get all carts.
         /// </summary>
-        /// <returns>A <see cref="IActionResult"/> that wraps the Cart content.</returns>
+        /// <returns>A <see cref="IActionResult"/>.</returns>
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult GetAllCarts()
         {
             this.logger.Log(LogLevel.Information, "Called: GET /Carts");
 
             return this.Ok();
+        }
+
+        /// <summary>
+        /// Get a cart by id.
+        /// </summary>
+        /// <param name="cartId">The cart to get.</param>
+        /// <returns>A <see cref="IActionResult"/>.</returns>
+        [HttpGet("{cartId}")]
+        public IActionResult GetCart(Guid cartId)
+        {
+            this.logger.Log(LogLevel.Information, "Called: GET /Carts");
+
+            return this.Ok();
+        }
+
+        /// <summary>
+        /// Create a cart.
+        /// </summary>
+        /// <returns>A <see cref="IActionResult"/>.</returns>
+        [HttpPost]
+        public IActionResult CreateCart()
+        {
+            this.logger.Log(LogLevel.Information, "Called: GET /Carts");
+
+            return this.Ok();
+        }
+
+        /// <summary>
+        /// Delete a cart.
+        /// </summary>
+        /// <param name="cartId">The cart id.</param>
+        /// <returns>A <see cref="IActionResult"/>.</returns>
+        [HttpDelete("{cartId}")]
+        public async Task<IActionResult> DeleteCart([FromRoute] Guid cartId)
+        {
+            this.logger.Log(LogLevel.Information, $"Called: Delete /Carts/{cartId}");
+
+            var cart = await this.cache.GetAsync<Cart>(Cart.GenerateCacheKey(cartId));
+
+            if (cart == null)
+            {
+                return this.NotFound($"Cart {cartId} not found.");
+            }
+            else
+            {
+                await this.cache.RemoveAsync(cart.CacheKey);
+                return this.NoContent();
+            }
         }
     }
 }
---
+++ Microservice.CartManager/Microservice.CartManager/Extensions/DistributedCacheExtensions.cs
@@ -0,0 +1,138 @@
+﻿namespace Microservice.CartManager.Extensions
+{
+    using System;
+    using System.Text;
+    using System.Text.Json;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Caching.Distributed;
+
+    /// <summary>
+    /// Extensions on the distributed cache interface for working with objects.
+    /// </summary>
+    public static class DistributedCacheExtensions
+    {
+        /// <summary>
+        /// Gets an object value from the cache.
+        /// </summary>
+        /// <typeparam name="T">The object type.</typeparam>
+        /// <param name="cache">The cache.</param>
+        /// <param name="key">The cache key.</param>
+        /// <returns>The object value.</returns>
+        public static T Get<T>(
+            this IDistributedCache cache,
+            string key)
+        {
+            if (cache == null)
+            {
+                throw new ArgumentNullException(nameof(cache));
+            }
+
+            var bytes = cache.Get(key);
+
+            if (bytes == null)
+            {
+                return default;
+            }
+            else
+            {
+                var serialized = Encoding.UTF8.GetString(bytes);
+                return JsonSerializer.Deserialize<T>(serialized);
+            }
+        }
+
+        /// <summary>
+        /// Sets an object value in the cache.
+        /// </summary>
+        /// <typeparam name="T">The object type.</typeparam>
+        /// <param name="cache">The cache.</param>
+        /// <param name="key">The cache key.</param>
+        /// <param name="value">The object value.</param>
+        /// <param name="entryOptions">The entry expiration options.</param>
+        public static void Set<T>(
+            this IDistributedCache cache,
+            string key,
+            T value,
+            DistributedCacheEntryOptions entryOptions = null)
+        {
+            if (cache == null)
+            {
+                throw new ArgumentNullException(nameof(cache));
+            }
+
+            if (entryOptions == null)
+            {
+                entryOptions = new DistributedCacheEntryOptions();
+            }
+
+            var serialized = JsonSerializer.Serialize(value);
+            var bytes = Encoding.UTF8.GetBytes(serialized);
+
+            cache.Set(key, bytes, entryOptions);
+        }
+
+        /// <summary>
+        /// Gets an object value from the cache asynchronously.
+        /// </summary>
+        /// <typeparam name="T">The object type.</typeparam>
+        /// <param name="cache">The cache.</param>
+        /// <param name="key">The cache key.</param>
+        /// <param name="token">Optional. The <see cref="T:System.Threading.CancellationToken" /> used to propagate notifications that the operation should be canceled.</param>
+        /// <returns>The object value.</returns>
+        public static async Task<T> GetAsync<T>(
+            this IDistributedCache cache,
+            string key,
+            CancellationToken token = default)
+        {
+            if (cache == null)
+            {
+                throw new ArgumentNullException(nameof(cache));
+            }
+
+            var bytes = await cache.GetAsync(key, token);
+
+            if (bytes == null)
+            {
+                return default;
+            }
+            else
+            {
+                var serialized = Encoding.UTF8.GetString(bytes);
+                return JsonSerializer.Deserialize<T>(serialized);
+            }
+        }
+
+        /// <summary>
+        /// Sets an object value in the cache.
+        /// </summary>
+        /// <typeparam name="T">The object type.</typeparam>
+        /// <param name="cache">The cache.</param>
+        /// <param name="key">The cache key.</param>
+        /// <param name="value">The object value.</param>
+        /// <param name="entryOptions">The entry expiration options.</param>
+        /// <param name="token">Optional. The <see cref="T:System.Threading.CancellationToken" /> used to propagate notifications that the operation should be canceled.</param>
+        /// <returns>A <see cref="Task"/>.</returns>
+        public static async Task SetAsync<T>(
+            this IDistributedCache cache,
+            string key,
+            T value,
+            DistributedCacheEntryOptions entryOptions = null,
+            CancellationToken token = default)
+        {
+            if (cache == null)
+            {
+                throw new ArgumentNullException(nameof(cache));
+            }
+
+            if (entryOptions == null)
+            {
+                entryOptions = new DistributedCacheEntryOptions();
+            }
+
+            var serialized = JsonSerializer.Serialize(value);
+            var bytes = Encoding.UTF8.GetBytes(serialized);
+
+            await cache.SetAsync(key, bytes, entryOptions, token);
+        }
+    }
+}
